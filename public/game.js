73915475c2ab76ded8f22c303f877906ebaf6824
philipnilsson <philip.nilsson@jayway.com>
2013-07-13 23:14:42 +0200

    Refactoring

--- public/game.js
+++ public/game.js
@@ -14,6 +14,34 @@ function keyInputs() {
   }
 }
 
+var gameLogic = function(makeBlock, input, board) {
+ 
+  return makeBlock().flatMap(function(block) {
+    var init = new BlockState(block, board);
+  
+    if (init.collides()) {
+      init.isSet = true;
+      return Bacon.once(init);
+    }
+
+    var block = Bacon.update(
+       init,
+       [input.ups],    Block.move( 0, 0, 3),
+       [input.downs],  Block.move( 0, 0, 1),
+       [input.lefts],  Block.move(-1, 0, 0),
+       [input.rights], Block.move( 1, 0, 0),
+       [input.ts],     Block.move( 0, 1, 0),
+       [input.space],  function(st) { return st.down(); }
+    );
+
+    return block
+      .takeUntil(function(x) { return x.isSet; })
+      .flatMapEnd(function () { return gameLogic(makeBlock, input, board) } );
+  });
+};
+
+
+
 function tetris(drawing, input, makeBlock) {
   
   drawing.drawGameArea(w, h);
@@ -21,33 +49,8 @@ function tetris(drawing, input, makeBlock) {
   var board = new Board(10, 20);
   var blockList = new Bacon.Bus()
   
-  var game = function() {
-   
-    return makeBlock().flatMap(function(block) {
-      var init = new BlockState(block, board);
-    
-      if (init.collides()) {
-        init.isSet = true;
-        return Bacon.once(init);
-      }
-
-      var block = Bacon.update(
-         init,
-         [input.ups],    Block.move( 0, 0, 3),
-         [input.downs],  Block.move( 0, 0, 1),
-         [input.lefts],  Block.move(-1, 0, 0),
-         [input.rights], Block.move( 1, 0, 0),
-         [input.ts],     Block.move( 0, 1, 0),
-         [input.space],  function(st) { return st.down(); }
-      );
-
-      return block
-        .takeUntil(function(x) { return x.isSet; })
-        .flatMapEnd(game);
-    });
-  };
   
-  var g = game()
+  var g = gameLogic(makeBlock, input, board)
   g.onValue(function (block) { 
     if (block.isSet) 
       drawing.setBlock(block, board.set(block));
